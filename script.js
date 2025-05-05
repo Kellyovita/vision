ec723680af9c89c711c2abc377c45e68c1b28975
power hub <82532313+Kellyovita@users.noreply.github.com>
2025-05-05 16:24:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const toggleBtn = document.querySelector(".dark-toggle");
 toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark-mode");
 });
 
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("show");
+  navLinks.classList.toggle("show");
 });
 
 const backToTopButton = document.querySelector(".back-to-top");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-        backToTopButton.style.display = "block";
-    } else {
-        backToTopButton.style.display = "none";
-    }
+  if (window.scrollY > 300) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
 });
